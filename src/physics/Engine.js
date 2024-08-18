847eeae93be8bd4785b6ff1b7b10b041d4f3d5b5
m-hilani <148714322+m-hilani@users.noreply.github.com>
2024-08-18 11:20:35 +0300

    changed the throttle

--- src/physics/Engine.js
+++ src/physics/Engine.js
@@ -1,24 +1,25 @@
 import * as THREE from "three";
-import { radiansToDegrees,degreesToRadians } from "../Components/MathCalc";
+import { radiansToDegrees, degreesToRadians } from "../Components/MathCalc";
 
 class Engine {
-    constructor(power, maxRPM, efficiency,guifolder) {
-        this.power = power;      
-        this.maxRPM = maxRPM;    
-        this.efficiency = efficiency;  
-        this.currentRPM = 0; 
+  constructor(power, maxRPM, efficiency, guifolder) {
+    this.power = power;
+    this.maxRPM = maxRPM;
+    this.efficiency = efficiency;
+    this.currentRPM = 0;
+  }
 
-    }
+  setRPM(RPM) {
+    this.currentRPM = RPM;
+  }
 
-    setRPM(RPM) { 
-        this.currentRPM = RPM 
-    }
-
-    getThrustForce() { //
-        // console.log("theta: " + radiansToDegrees(theta));
-        const thrust = this.power * this.efficiency * (this.currentRPM / this.maxRPM);
-        return new THREE.Vector3(thrust,0,thrust);
-    }
+  getThrustForce() {
+    //
+    // console.log("theta: " + radiansToDegrees(theta));
+    const thrust =
+      this.power * this.efficiency * (this.currentRPM ** 2 / this.maxRPM ** 2);
+    return new THREE.Vector3(thrust, 0, thrust);
+  }
 }
 
 export default Engine;
